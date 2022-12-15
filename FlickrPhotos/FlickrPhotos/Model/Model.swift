cd7b3c37e67b91f2d79a33b32b8daf65cc4879b3
Andrew Jones <ajones20324@gmail.com>
2022-12-15 12:39:41 -0600

    Flickr puppy photo search
    Flickr puppy photo search built with Swift, MVVM and URLSession

---
+++ FlickrPhotos/FlickrPhotos/Model/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  FlickrPhotoSearch
+//
+//  Created by Andrew Ikenna Jones on 12/13/21.
+//
+
+import Foundation
+
+struct FlickrPhoto: Codable {
+    let title: String
+    let id: String
+    let server: String
+    let secret: String
+    
+    var thumbUrl: URL {
+        return URL(string: String(format: "https://live.staticflickr.com/%@/%@_%@_t.jpg", server, id, secret))!
+    }
+    
+    var fullSizeUrl: URL {
+        return URL(string: String(format: "https://live.staticflickr.com/%@/%@_%@_b.jpg", server, id, secret))!
+    }
+}
+
+struct FlickrPublicPhoto {
+    
+}
